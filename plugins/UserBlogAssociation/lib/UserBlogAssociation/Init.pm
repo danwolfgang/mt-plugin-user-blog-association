7a7ac9dd2bc5a93958d89178965ddc9307fa5631
Dan Wolfgang <dan@uinnovations.com>
2010-12-15 14:15:39 -0500

    Initial commit.

---
+++ plugins/UserBlogAssociation/lib/UserBlogAssociation/Init.pm
@@ -0,0 +1,73 @@
+package UserBlogAssociation::Init;
+
+use strict;
+use Sub::Install;
+
+use MT::Util qw( remove_html is_url );
+
+
+sub init_app {
+    my ( $cb, $app ) = @_;
+    return
+      unless $app->isa('MT::App')
+          && ( $app->can('query') || $app->can('param') );
+
+    Sub::Install::reinstall_sub({ 
+        code => \&_commenter_loggedin, 
+        into => 'MT::App::Community', 
+        as   => 'commenter_loggedin', 
+    });
+}
+
+# The following is lifted from the Community Pack 1.63, from
+# MT::App::Community. commenter_loggedin is called at the end of the user
+# log-in process, so I'm overriding that to add the ability to tie a user
+# and blog together.
+sub _commenter_loggedin {
+    my $app = shift;
+    my $q   = $app->param;
+    my ($commenter, $commenter_blog_id) = @_;
+
+    # Create the user-blog association before returning the user to 
+    # wherever they came from.
+    require UserBlogAssociation::Plugin;
+    UserBlogAssociation::Plugin::_create_association(
+        $commenter_blog_id,
+        $commenter
+    );
+
+    my $return_to = $q->param('return_to') || $q->param('return_url');
+    if ( $return_to ) {
+        $return_to = remove_html($return_to);
+        $return_to =~ s/#.+//;
+        return $app->errtrans('Invalid request.')
+          unless is_url( $return_to );
+    }
+
+    my $url;
+    $app->make_commenter_session($commenter);
+    if ($return_to) {
+        $url = $return_to . '#_login';
+    }
+    else {
+        if ($commenter_blog_id) {
+            my $blog
+                = $app->model('blog.community')->load( $q->param('blog_id') )
+                or return $app->errtrans("Invalid parameter");
+            $url = $blog->site_url . '#_login';
+        }
+        else {
+            my $cfg = $app->config;
+            $url
+                = $cfg->ReturnToURL
+                ? $cfg->ReturnToURL
+                : $app->uri( mode => 'edit' );
+        }
+    }
+    return $url if $url;
+    $app->SUPER::commenter_loggedin(@_);
+}
+
+1;
+
+__END__
---
+++ plugins/UserBlogAssociation/lib/UserBlogAssociation/Plugin.pm
@@ -0,0 +1,66 @@
+package UserBlogAssociation::Plugin;
+
+use strict;
+
+sub blog_config_template {
+    my ($plugin, $param, $scope) = @_;
+
+    my @roles = MT->model('role')->load();
+    $param->{roles} = \@roles;
+    
+    my $html = <<'HTML';
+<mtapp:Setting
+    id="select-role"
+    label="Select Role"
+    hint="Select a role to assign to users when they interact with this blog."
+    show_hint="1">
+    <select name="saved_role_id">
+        <option value="">None</option>
+    <mt:Loop name="roles">
+        <option value="<mt:Var name="id">"<mt:If name="saved_role_id" eq="$id"> selected="selected"</mt:If>><mt:Var name="name"></option>
+    </mt:Loop>
+    </select>
+</mtapp:Setting>
+HTML
+    return $html;
+}
+
+sub post_save {
+    my ($cb, $app, $obj, $original) = @_;
+    # Validate the user.
+    use MT::App::Community;
+    my $user = MT::App::Community::_login_user_commenter($app)
+        or return $app->errtrans("Login required");
+
+    # Create the association
+    _create_association($app->param('blog_id'), $user);
+}
+
+sub _create_association {
+    my ($blog_id, $user) = @_;
+    # Just give up if a blog ID isn't supplied -- no way to create a user-
+    # blog association if there's no blog!
+    my $blog = MT->model('blog')->load($blog_id);
+    return unless $blog;
+
+    # Check if a role has been marked to be used to create the user-blog 
+    # association. If not, just give up.
+    my $plugin = MT->component('userblogassociation');
+    my $role_id = $plugin->get_config_value(
+        'saved_role_id', 
+        'blog:'.$blog_id
+    );
+    return unless $role_id;
+
+    # Try to load the role from the saved role_id.
+    my $role = MT->model('role')->load($role_id);
+    return unless $role;
+    
+    # Create the user-role-blog association.
+    require MT::Association;
+    MT::Association->link( $user, $role, $blog );
+}
+
+1;
+
+__END__
